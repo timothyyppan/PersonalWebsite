0e97b73aef632c15c5a5a577c0b8fd4dd28b6fa9
timothyyppan <124206804+timothyyppan@users.noreply.github.com>
2023-09-12 21:04:50 -0400

    cleaned

--- option-menu.js
+++
@@ -1,8 +0,0 @@
-let menuOptions = document.getElementsByClassName("menu-options");
-
-menuOptions.addEventListener("click", function(){
-    let menu = document.getElementById("menu");
-    if(menu.style.right === 0){
-        menu.style.right = "-100%";
-    }
-});
